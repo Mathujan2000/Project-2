ae098b3a606c106ed7e3338a5eb2e8e04facfd2f
Hassan <ps197137@summacollege.nl>
2023-06-13 15:14:00 +0200

    Dit is een nieuwe commit

--- Webpage/hoofdpagina.php
+++ Webpage/hoofdpagina.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,39 +8,58 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Partij</title>
 </head>
+
 <body>
     <div class="container">
         <header>
-            HEADER
+            <h1> De Homepagina</h1>
         </header>
-        <div id="logo"> 
+        <div id="logo">
             <img src="..//Webpage/images/pvda.png" alt="logo" class="partijlogo">
-            </div>
+
+        </div>
+
+
         <nav>
-            NAV
+
+            <ul>
+                <li class="menu"><a href="hoofdpagina.php">Home</a></li>
+                <li class="menu"><a href="#">Nieuws</a></li>
+                <li class="menu"><a href="#">Leden</a></li>
+            </ul>
+
         </nav>
         <div id="calltoaction1">
-            CA1
-            <div id="ledentekst">
 
-<h3> Schrijf je hier bij ons in!</h3>
-    </div>
-            <div id="leden">
-            <img src="..//Webpage/images/meedoen.jpg" alt="meedoen" class="meedoenfoto">
+            <div id="ledentekst">
+                <div class="text-container">
+                    <h1> Schrijf je hier bij ons in!</h1>
+                </div>
+                <div id="leden">
+                    <img src="..//Webpage/images/meedoen.jpg" alt="meedoen" class="cafoto">
+                </div>
             </div>
         </div>
         <div id="calltoaction2">
-            CA2
+            <div id="ledentekst2">
+                <div class="text-container2">
+                    <h1> Bekijk hier onze leden! <a href="leden.php"></a></h1>
+                </div>
+                <div id="leden2">
+                    <img src="..//Webpage/images/leden.jpg" alt="meedoen" class="cafoto">
+                </div>
+            </div>
         </div>
         <div id="nieuwsbericht1">
-            NB1
+
         </div>
         <div id="nieuwsbericht2">
-            NB2
+
         </div>
         <footer>
-            FOOTER
+
         </footer>
     </div>
 </body>
+
 </html>
